b1fc1603c2bbff1bf890b372b2d9edd97b8f3412
Doug Schmidt <doug.schmidt@aquaticinformatics.com>
2020-02-03 15:58:22 -0800

    Issue-46 - Allow the tester to parse ZIPs-with-attachments too

--- src/PluginTester/Tester.cs
+++ src/PluginTester/Tester.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using Common;
 using FieldDataPluginFramework;
+using FieldDataPluginFramework.Context;
 using FieldDataPluginFramework.Results;
 using FieldDataPluginFramework.Serialization;
+using Humanizer;
 using log4net;
 using ServiceStack;
 using ServiceStack.Text;
@@ -47,41 +50,115 @@ namespace PluginTester
 
         private void ParseOneFile(string path)
         {
-            using (var stream = LoadDataStream(path))
+            var dataBytes = LoadDataStream(path);
+
+            var appender = new FieldDataResultsAppender
             {
-                var appender = new FieldDataResultsAppender
+                UtcOffset = Context.LocationUtcOffset
+            };
+
+            var locationInfo = !string.IsNullOrEmpty(Context.LocationIdentifier)
+                ? appender.CreateDummyLocationInfoByIdentifier(Context.LocationIdentifier)
+                : null;
+
+            appender.ForcedLocationInfo = locationInfo;
+            appender.AppendedResults.PluginAssemblyQualifiedTypeName = Plugin.GetType().AssemblyQualifiedName;
+
+            try
+            {
+                var result = ParseFile(dataBytes, appender, locationInfo);
+
+                SaveAppendedResults(path, appender.AppendedResults);
+
+                SummarizeResults(path, result, appender.AppendedResults);
+            }
+            catch (ExpectedException)
+            {
+                throw;
+            }
+            catch (Exception exception)
+            {
+                Log.Error("Plugin has thrown an error", exception);
+
+                throw new ExpectedException($"Unhandled plugin exception: {exception.Message}");
+            }
+        }
+
+        private ResultOrException ParseFile(byte[] dataBytes, FieldDataResultsAppender appender, LocationInfo locationInfo)
+        {
+            // Many plugins, like FlowTracker2, accept a ZIP archive that also matches the Zip-with-attachments pattern.
+            // So always try the plugin first with the actual data
+            if (TryParseFile(dataBytes, appender, locationInfo, out var directAttempt)
+                && IsSuccessfulParse(directAttempt.Result))
+                return new ResultOrException
                 {
-                    UtcOffset = Context.LocationUtcOffset
+                    Result = directAttempt.Result
                 };
 
-                var locationInfo = !string.IsNullOrEmpty(Context.LocationIdentifier)
-                    ? appender.CreateDummyLocationInfoByIdentifier(Context.LocationIdentifier)
-                    : null;
+            ResultOrException zipAttempt = null;
 
-                appender.ForcedLocationInfo = locationInfo;
-                appender.AppendedResults.PluginAssemblyQualifiedTypeName = Plugin.GetType().AssemblyQualifiedName;
-
-                try
+            if (ZipLoader.TryParse(dataBytes, out var zipWithAttachments)
+                && TryParseFile(zipWithAttachments.PluginDataBytes, appender, locationInfo, out zipAttempt)
+                && IsSuccessfulParse(zipAttempt.Result))
+                return new ResultOrException
                 {
-                    var result = string.IsNullOrEmpty(Context.LocationIdentifier)
-                        ? Plugin.ParseFile(stream, appender, Logger)
-                        : Plugin.ParseFile(stream, locationInfo, appender, Logger);
+                    Result = zipAttempt.Result,
+                    Attachments = zipWithAttachments.Attachments
+                };
 
+            if (zipWithAttachments != null)
+            {
+                if (zipAttempt == null)
+                    throw new ArgumentNullException(nameof(zipAttempt));
 
-                    SaveAppendedResults(path, appender.AppendedResults);
-
-                    SummarizeResults(path, result, appender.AppendedResults);
-                }
-                catch (ExpectedException)
+                // Something failed inside the Zip parser
+                return new ResultOrException
                 {
-                    throw;
-                }
-                catch (Exception exception)
-                {
-                    Log.Error("Plugin has thrown an error", exception);
+                    Result = zipAttempt.Result,
+                    Attachments = zipWithAttachments.Attachments,
+                    Exception = zipAttempt.Exception
+                };
+            }
 
-                    throw new ExpectedException($"Unhandled plugin exception: {exception.Message}");
+            return new ResultOrException
+            {
+                Result = directAttempt.Result,
+                Exception = directAttempt.Exception
+            };
+        }
+
+        private static bool IsSuccessfulParse(ParseFileResult result)
+        {
+            return result.Status != ParseFileStatus.CannotParse;
+        }
+
+        private class ResultOrException
+        {
+            public ParseFileResult Result { get; set; }
+            public List<Attachment> Attachments { get; set; }
+            public Exception Exception { get; set; }
+        }
+
+        private bool TryParseFile(byte[] dataBytes, FieldDataResultsAppender appender, LocationInfo locationInfo, out ResultOrException resultOrException)
+        {
+            resultOrException = new ResultOrException();
+
+            try
+            {
+                using (var stream = new MemoryStream(dataBytes))
+                {
+                    resultOrException.Result = locationInfo != null
+                        ? Plugin.ParseFile(stream, locationInfo, appender, Logger)
+                        : Plugin.ParseFile(stream, appender, Logger);
                 }
+
+                return true;
+            }
+            catch (Exception exception)
+            {
+                resultOrException.Exception = exception;
+
+                return false;
             }
         }
 
@@ -99,17 +176,17 @@ namespace PluginTester
             File.WriteAllText(savePath, appendedResults.ToJson().IndentJson());
         }
 
-        private void SummarizeResults(string path, ParseFileResult result, AppendedResults appendedResults)
+        private void SummarizeResults(string path, ResultOrException resultOrException, AppendedResults appendedResults)
         {
             try
             {
-                if (result.Status == Context.ExpectedStatus)
+                if (resultOrException.Result?.Status == Context.ExpectedStatus)
                 {
-                    SummarizeExpectedResults(path, result, appendedResults);
+                    SummarizeExpectedResults(path, resultOrException, appendedResults);
                 }
                 else
                 {
-                    SummarizeFailedResults(path, result, appendedResults);
+                    SummarizeFailedResults(path, resultOrException, appendedResults);
                 }
 
             }
@@ -127,60 +204,80 @@ namespace PluginTester
             }
         }
 
-        private void SummarizeExpectedResults(string path, ParseFileResult result, AppendedResults appendedResults)
+        private void SummarizeExpectedResults(string path, ResultOrException resultOrException, AppendedResults appendedResults)
         {
-            if (result.Parsed)
+            var result = resultOrException.Result;
+
+            var attachmentSummary = GetAttachmentSummary(resultOrException.Attachments);
+
+            if (resultOrException.Exception == null && result.Parsed)
             {
                 if (!appendedResults.AppendedVisits.Any())
                 {
-                    throw new ExpectedException($"'{path}' was parsed but no visits were appended.");
+                    throw new ExpectedException($"'{path}' was parsed{attachmentSummary} but no visits were appended.");
                 }
 
-                Log.Info($"Successfully parsed {appendedResults.AppendedVisits.Count} visits from '{path}'.");
+                Log.Info($"Successfully parsed {appendedResults.AppendedVisits.Count} visits{attachmentSummary} from '{path}'.");
             }
             else
             {
-                var actualError = result.ErrorMessage ?? string.Empty;
+                var actualError = resultOrException.Exception?.Message ?? result.ErrorMessage ?? string.Empty;
                 var expectedError = Context.ExpectedError ?? string.Empty;
 
                 if (!actualError.Equals(expectedError))
                     throw new ExpectedException(
-                        $"Expected an error message of '{expectedError}' but received '{actualError}' instead while parsing '{path}'.");
+                        $"Expected an error message of '{expectedError}' but received '{actualError}' instead while parsing '{path}'{attachmentSummary}.");
 
-                Log.Info($"ParsedResult.Status == {Context.ExpectedStatus} with Error='{Context.ExpectedError}' as expected when parsing '{path}'.");
+                Log.Info($"ParsedResult.Status == {Context.ExpectedStatus} with Error='{Context.ExpectedError}' as expected when parsing '{path}'{attachmentSummary}.");
             }
         }
 
-        private void SummarizeFailedResults(string path, ParseFileResult result, AppendedResults appendedResults)
+        private void SummarizeFailedResults(string path, ResultOrException resultOrException, AppendedResults appendedResults)
         {
+            if (resultOrException.Exception != null)
+                throw new ExpectedException($"Can't parse '{path}'. {resultOrException.Exception.Message}");
+
+            var result = resultOrException.Result;
+
+            var attachmentSummary = GetAttachmentSummary(resultOrException.Attachments);
+
             if (result.Parsed)
                 throw new ExpectedException(
-                    $"'{path}' was parsed successfully with {appendedResults.AppendedVisits.Count} visits appended when {Context.ExpectedStatus} was expected.");
+                    $"'{path}' was parsed successfully with {appendedResults.AppendedVisits.Count} visits{attachmentSummary} appended when {Context.ExpectedStatus} was expected.");
 
             if (result.Status != ParseFileStatus.CannotParse)
                 throw new ExpectedException(
-                    $"Result: '{path}' Parsed={result.Parsed} Status={result.Status} ErrorMessage={result.ErrorMessage}");
+                    $"Result: '{path}' Parsed={result.Parsed} Status={result.Status} ErrorMessage={result.ErrorMessage}{attachmentSummary}");
 
-            if (!string.IsNullOrEmpty(result.ErrorMessage))
+            var errorMessage = resultOrException.Exception?.Message ?? result.ErrorMessage;
+
+            if (!string.IsNullOrEmpty(errorMessage))
             {
-                throw new ExpectedException($"Can't parse '{path}'. {result.ErrorMessage}");
+                throw new ExpectedException($"Can't parse '{path}'. {errorMessage}{attachmentSummary}");
             }
 
-            throw new ExpectedException($"File '{path}' is not parsed by the plugin.");
+            throw new ExpectedException($"File '{path}'{attachmentSummary} is not parsed by the plugin.");
         }
 
-        private Stream LoadDataStream(string path)
+        private static string GetAttachmentSummary(List<Attachment> attachments)
+        {
+            if (attachments == null || !attachments.Any())
+                return string.Empty;
+
+            var totalByteSize = attachments
+                .Sum(a => a.ByteSize);
+
+            return $" and {totalByteSize.Bytes().Humanize("#.#")} in {attachments.Count} attachments: {string.Join(", ", attachments.Select(a => $"'{Path.GetFileName(a.Path)}' ({a.ByteSize.Bytes().Humanize("#.#")})"))}";
+        }
+
+        private byte[] LoadDataStream(string path)
         {
             if (!File.Exists(path))
                 throw new ExpectedException($"Data file '{path}' does not exist.");
 
             Log.Info($"Loading data file '{path}'");
 
-            using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            using (var reader = new BinaryReader(stream))
-            {
-                return new MemoryStream(reader.ReadBytes((int)stream.Length));
-            }
+            return File.ReadAllBytes(path);
         }
 
         private IFieldDataPlugin LoadPlugin()
